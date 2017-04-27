63ceabe5e63c1eeac6e24e87327003e75b118832
munehiro.ochi <munehiro.ochi@nijibox.co.jp>
2017-04-27 17:21:32 +0900

    Initial Commit

---
+++ TalkRoom1Tests/TalkRoom1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TalkRoom1Tests.swift
+//  TalkRoom1Tests
+//
+//  Created by nijibox on 2017/04/27.
+//  Copyright © 2017年 nijibox. All rights reserved.
+//
+
+import XCTest
+@testable import TalkRoom1
+
+class TalkRoom1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
